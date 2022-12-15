306fc217d0ecd9d64940eba8137ac29c33fd26e6
edenkozlov <96761592+edenkozlov@users.noreply.github.com>
2022-12-15 13:58:08 -0500

    !

--- src/App.js
+++ src/App.js
@@ -204,20 +204,20 @@ function App() {
         image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null}
       >
         
-        <s.SpacerSmall />
+        <s.SpacerXSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
           
-          <s.SpacerLarge />
+          <s.SpacerXSmall />
           <s.Container
             flex={2}
             jc={"center"}
             ai={"center"}
             style={{
-              backgroundColor: "var(--accent)",
+              backgroundColor: "transparent",
               padding: 24,
               borderRadius: 15,
-              border: "4px solid var(--secondary)",
-              boxShadow: "0px 5px 11px 2px rgba(0,0,0,0.7)",
+              
+              
             }}
           ><s.Container flex={1} jc={"center"} ai={"center"}>
          <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
@@ -226,6 +226,7 @@ function App() {
             alt={"example"}
             src={"/config/images/example.gif"}
             style={{ transform: "scaleX(1)" }}
+            
           />
         </s.Container>
             <s.TextTitle
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -7,7 +7,7 @@ export const Screen = styled.div`
   background-size: cover;
   background-position: center;
   width: 100%;
-  min-height: 100vh;
+  min-height: 115vh;
   display: flex;
   flex-direction: column;
 `;
